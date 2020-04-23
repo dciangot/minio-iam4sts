eec45756f1efea5d3b08140594b0ce8fd7adcbc0
dciangot <diego.ciangottini@gmail.com>
2020-04-23 11:12:43 +0200

    working version

---
+++ README.md
@@ -0,0 +1,11 @@
+# Quick start
+
+- Rertrieve ID_Token from the provider and set it in the `TOKEN` env var
+- `pip3 install --user -r requirements.txt`
+- `curl https://rclone.org/install.sh | sudo bash`
+- `TOKEN=$TOKEN python3 main.py`
+    - this will use the id_token provided retrieve minio credentials
+    - check if the user bucket exists and create it if not
+    - mount all the user buckets in /tmp/<username>
+    - do some operation
+    - unmount the volume and exit
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from minio import Minio
+from minio.error import (ResponseError, BucketAlreadyOwnedByYou,
+                         BucketAlreadyExists)
+import jwt
 import requests
 import xmltodict
+import os
+import glob
 
-token = "eyJhbGciOiJSUzI1NiIsImtpZCI6ImJhZWM3ZDZkNjk0OGM4YjFmMDQxM2M4MWE5NmIzODEwNWE2MTMzYzMifQ.eyJpc3MiOiJodHRwOi8vOTAuMTQ3LjE3NC45Mjo1NTU2L2RleCIsInN1YiI6IkNpUTBOelkyTkRKa055MHhaRFJpTFRSbU56Y3RPRGN5TlMxbFl6aGhNMlExTkRjME5qTVNCV1J2WkdGeiIsImF1ZCI6Im1pbmlvLWNuYWYiLCJleHAiOjE1ODc2NTg5MDgsImlhdCI6MTU4NzU3MjUwOCwibm9uY2UiOiJBOWxvT3dzUTN5ZDY0c1VGIiwiYXRfaGFzaCI6Il9RVG9aeVhVNWJKVGtSc1FKUjJIckEiLCJlbWFpbCI6IkRhbmllbGUuU3BpZ2FAcGcuaW5mbi5pdCIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJuYW1lIjoiRGFuaWVsZSBTcGlnYSJ9.ZmYNiV0peKOAnk08HqeKaYyYUmooNxk2HZQgkVMLtJ22lxRD3oqGz4gP2mMhkftRn5BX0bAATF3tSZIGmZoI7RKhN-izWspUx-fwhLEFg2PhvXzxRx9HXa7nmkhx3a8aZJERqeZj_ETqp_S_ziqiIASx8-UHG6BTXWJZX4u8AGsLrJRqyXcKdFU-LAYdARgae64yR1QL8zeCnHf_vuqNf-91YIpCWdYGCWRdlTCwxP5E53JgSn1oGSC-nPr6_Ddlg8hPbjxcE9FJMlhnKSuXPnfUlZC78fwh-RAjCMtBf7lyJlu1ZMEaR6ofj1YcvcEXkP6I3lbFgR4wcNCgm_L87A"
+token = os.getenv("TOKEN")
 
 r = requests.post("http://131.154.97.121:9000", data={
     'Action': "AssumeRoleWithWebIdentity",
@@ -14,6 +19,68 @@ print(r.status_code, r.reason)
 
 tree = xmltodict.parse(r.content)
 
-credenstials = dict(tree['AssumeRoleWithWebIdentityResponse']['AssumeRoleWithWebIdentityResult']['Credentials']).keys()
-# [u'SecretAccessKey', u'AccessKeyId', u'Expiration', u'SessionToken']
+credenstials = dict(tree['AssumeRoleWithWebIdentityResponse']
+                    ['AssumeRoleWithWebIdentityResult']['Credentials'])
 
+# Get username from token and check if a folder for the mount is ready
+username = jwt.decode(token, verify=False)['email'].lower().split("@")[0]
+directory = "/tmp/" + username
+
+if not os.path.exists(directory):
+    os.makedirs(directory)
+
+# Initialize Minio client
+minioClient = Minio(
+    '131.154.97.121:9000',
+    access_key=credenstials['AccessKeyId'],
+    secret_key=credenstials['SecretAccessKey'],
+    session_token=credenstials['SessionToken'],
+    secure=False
+)
+
+# Make a bucket with the make_bucket API call.
+try:
+    minioClient.make_bucket(username)
+except BucketAlreadyOwnedByYou:
+    pass
+except BucketAlreadyExists:
+    pass
+except ResponseError as err:
+    raise err
+
+# Write rclone config file in $PWD/<username>.conf
+config = """
+[%s]
+type = s3
+provider = Minio
+env_auth = false
+access_key_id = %s
+secret_access_key = %s
+session_token = %s
+endpoint = http://%s:9000
+""" % (
+    username,
+    credenstials['AccessKeyId'],
+    credenstials['SecretAccessKey'],
+    credenstials['SessionToken'],
+    '131.154.97.121'
+    )
+
+with open("%s.conf" % username, "w") as conf_file:
+    conf_file.write(config)
+
+# Unmount volume if already present
+myCmd = os.popen('fusermount -u /tmp/%s' % username).read()
+print(myCmd)
+
+# Mount all user buckets
+myCmd = os.popen('rclone --config %s.conf mount --daemon --vfs-cache-mode full --no-modtime %s: /tmp/%s && sleep 2' % (username, username, username)).read()
+print(myCmd)
+
+# List contents of user buckets
+myCmd = os.popen('ls -ltrh /tmp/%s/*/' % (username)).read()
+print(myCmd)
+
+# Unmount before exit
+myCmd = os.popen('sleep 2 && fusermount -u /tmp/%s' % username).read()
+print(myCmd)
